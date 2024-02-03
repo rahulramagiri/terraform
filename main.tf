403efcd7451caf807b4ac2605f77e0db32e709c8
rahulramagiri <rahulramagiri8@gmail.com>
2024-02-03 14:00:56 +0530

    s3 terraform

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "rahul_bucket" {
+  bucket = "bucket-from-terraform"
+}
